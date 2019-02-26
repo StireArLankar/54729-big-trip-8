525dab5c23b25bbf51678dbf841ba515d8ef5f18
StireArLankar <stire.ru@gmail.com>
2019-02-26 17:12:10 +0300

    refactor reducer in render-trip-points

--- src/components/main/render-trip-points.js
+++ src/components/main/render-trip-points.js
@@ -2,13 +2,25 @@ import dayTemplate from './day-template';
 
 const container = document.querySelector(`.trip-points`);
 
+const printDate = (date) => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  return `${addPrettyZeros(day)}.${addPrettyZeros(month)}`;
+};
+
+const addPrettyZeros = (number) => {
+  return number < 10 ? `0${number}` : `${number}`;
+};
+
 function renderTripPoints(points) {
-  const days = points.reduce((acc, cur) => {
+  const days = points.reduce((acc, cur, index) => {
     const lastIndex = acc.length - 1;
-    if (lastIndex === -1 || acc[lastIndex].date !== `${cur.date.start.getMonth() + 1}.${cur.date.start.getDate()}`) {
+    const curDate = printDate(cur.date.start);
+
+    if (index === 0 || acc[lastIndex].date !== curDate) {
       const obj = {
         index: acc.length + 1,
-        date: `${cur.date.start.getDate()}.${cur.date.start.getMonth() + 1}`,
+        date: curDate,
         points: [cur]
       };
       acc.push(obj);
